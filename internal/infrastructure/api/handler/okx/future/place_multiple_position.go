349181f2475a7388555960d5488946d22dd37859
A657031 A657031 <a657031@AR8040657031N1.local>
2024-02-18 00:32:44 +0700

    add place multiple assets handler

--- internal/infrastructure/api/handler/okx/future/place_multiple_position.go
+++ internal/infrastructure/api/handler/okx/future/place_multiple_position.go
@@ -1,21 +1,28 @@
 package handler
 
 import (
+	"net/http"
+	service "tradetoolv2/internal/app/service/okx/future"
+	helper "tradetoolv2/internal/helper/service_response"
 	"tradetoolv2/internal/infrastructure/api/handler/okx/future/request"
 
 	"github.com/labstack/echo/v4"
 )
 
 type IPlaceMultiplePositionHandler interface {
-	GetBody(c echo.Context) *request.PlaceMultiplePositionHandlerRequest
 	Handler(c echo.Context) error
 }
 
 type placeMultiplePositionHandler struct {
+	service service.IOkxFutureService
 }
 
-func NewPlaceMultiplePositionHandler() IPlaceMultiplePositionHandler {
-	return &placeMultiplePositionHandler{}
+func NewPlaceMultiplePositionHandler(
+	service service.IOkxFutureService,
+) IPlaceMultiplePositionHandler {
+	return &placeMultiplePositionHandler{
+		service,
+	}
 }
 
 func (p *placeMultiplePositionHandler) GetBody(c echo.Context) *request.PlaceMultiplePositionHandlerRequest {
@@ -23,5 +30,35 @@ func (p *placeMultiplePositionHandler) GetBody(c echo.Context) *request.PlaceMul
 }
 
 func (p *placeMultiplePositionHandler) Handler(c echo.Context) error {
-	return nil
+
+	body := new(request.PlaceMultiplePositionHandlerRequest)
+	err := c.Bind(body)
+	if err != nil {
+		return c.JSON(
+			http.StatusBadRequest,
+			&helper.CommonResponse{
+				Code:    helper.FailCode,
+				Message: err.Error(),
+			},
+		)
+	}
+
+	err = p.service.PlaceMultiplePosition(body)
+	if err != nil {
+		return c.JSON(
+			http.StatusInternalServerError,
+			&helper.CommonResponse{
+				Code:    helper.FailCode,
+				Message: err.Error(),
+			},
+		)
+	}
+	return c.JSON(
+		http.StatusOK,
+		helper.CommonResponse{
+			Code:    helper.SuccessCode,
+			Message: "Success",
+			Data:    nil,
+		},
+	)
 }
